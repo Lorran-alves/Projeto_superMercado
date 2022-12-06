48910ee40910532db3f17aeb57dbbc5347cb419f
Lorran-alves <lorrangamer81@gmail.com>
2022-12-06 18:54:37 -0300

    quase lá em requisitar as paginas via ajax

--- editar_produto_admin.php
+++ editar_produto_admin.php
@@ -1,5 +1,7 @@
 <?php 
-
+if($_GET['id_produto']){
+    $acao = $_GET['id_produto'];
+}
 // $acao = 'editarProduto';
 
 require "area_controle.php"; 
@@ -8,13 +10,23 @@ require "area_controle.php";
 // echo $_GET['id_produto'];
 
 
+echo 'caiu aqui';
+echo '<pre>';
+print_r($_FILES);
+echo '</pre>';
+echo '<br>';
+
+echo '<pre>';
+print_r($_POST);
+echo '</pre>';
 ?>
 
 
 <!-- area odne o produto irá ser editado -->
-<?php if($_GET['area'] == "editar"){?>
+<?php if($_GET['area'] == "editars"){?>
         
     <form action="area_controle.php?acao=editarProduto&id_produto=<?=$_GET['id_produto']?>" method="POST" class="form-admin" enctype="multipart/form-data">
+    
         <section class="row">
             <section class="form-campo1 coluna">
                 <label for="">Escolha a imagem do produto:</label>
@@ -58,6 +70,53 @@ require "area_controle.php";
     </form> 
         
 <?}?>
+<?php if($_GET['area'] == "editar"){?>
+        <section class="coluna">
+        <form class="form-admin" enctype="multipart/form-data" method="POST">
+        <section class="row">
+            <section class="form-campo1 coluna">
+                <label for="">Escolha a imagem do produto:</label>
+                <input type="file" name="arquivo" id="inputArquivo">
+                <label for="">Digite a descrição do produto:</label>
+                <textarea id="descricao" name="descricao" cols="45" rows="10"  placeholder="<?=$produtoEditar[0]['descricao']?>"></textarea>
+            </section>
+                <section class="form-campo2 coluna">  
+                    <label for="">Digite o nome do produto:</label>
+                    <input id='nome' type="text" placeholder="nome do produto" name="nome" value="<?=$produtoEditar[0]['nome']?>">
+
+                    <label for="">Digite a categoria do produto:</label>              
+                    <select name="categoria" id="categoria" >
+                        <optgroup>
+                            <option value="vazio" disabled selected>Selecione a categoria</option>
+                            <?php foreach($categorias as $indece => $categoria){?>
+                                <?if($categoria['nome_categoria'] == $produtoEditar[0]['categoria']){?>
+                                    <option value="<?=$categoria['nome_categoria']?>" selected><?=$categoria['nome_categoria']?></option>
+                                <?}else{?>
+                                    <option value="<?=$categoria['nome_categoria']?>"><?=$categoria['nome_categoria']?></option>
+                                <?}?>
+                            <?}?>
+                        </optgroup>
+                    </select>
+                </section>
+                <section class="form-campo3">
+
+                    <label for="">Digite a quantidade em estoque do produto:</label>
+                    <input id='quantidade' type="number" placeholder="Quantidade do estoque" name="estoque" min="1" value="<?=$produtoEditar[0]['estoque']?>"> 
+
+                    <label for="">Digite a valor do produto:</label>
+                    <input id='valor' type="text" oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1');" placeholder="Digite o valor do produto" name="preco" value="<?=$produtoEditar[0]['preco']?>">
+                </section>
+        </section>
+    </form> 
+    <section class="row">
+            <input type="submit" value="Salvar" class="botao-salvar" name= 'button' onclick="verificaMetodo('Salvar', <?=$_GET['id_produto']?>)">
+            <input type="submit" value="Remover" class="botao-remover" name= 'button' onclick="verificaMetodo('Remover', <?=$_GET['id_produto']?>)">
+             <input type="submit" value="Cancelar" class="botao-cancelar" name= 'button'  onclick="verificaMetodo('Cancelar', <?=$_GET['id_produto']?>)">
+        </section>
+        </section>
+       
+            
+    <?}?>
 
 <!-- area de escolher o produto a ser editado -->
 <?php if($_GET['area'] == "escolher"){?>
--- js/script.js
+++ js/script.js
@@ -16,7 +16,6 @@ function requisitarPagina(id, url){
 }
 function verificaFiltro(id){ // pesquisa admin 
     let parametro = document.querySelector('#'+id)
-    console.log(parametro.value)
 
     if(id == 'categoria'){
         requisitarPagina("pesquisa_admin", `pesquisa_admin.php?filtro=categoria&categoria=${parametro.value}`)
@@ -95,7 +94,6 @@ function calculaPedido(estoque, idProduto, indece){
     window.location = "area_controle.php?acao=verificarQuantidadePedido&id_produto="+idProduto+"&quantidade="+ qtd
 }
 function editarProduto(id = ''){
-    console.log('alterou o valor');
     let valorEscolhido = document.querySelector(`#${id}`)
     console.log(valorEscolhido.value);
     if(id != ''){
@@ -103,13 +101,56 @@ function editarProduto(id = ''){
     }
     
 }
+function verificaMetodo(metodo, id){
+    
+    let  inputArquivo = document.getElementById("inputArquivo");
+    let descricao = document.querySelector("#descricao").value
+    let nome = document.querySelector("#nome").value
+    let categoria = document.querySelector("#categoria").value
+    let valor = document.querySelector("#valor").value
+    let quantidade = document.querySelector("#quantidade").value
+    console.log(inputArquivo.files)
+    
+    if(inputArquivo.files[0]['name'] != '' && descricao != '' && nome != '' && categoria != 'vazio' && valor != 'vazio' && quantidade != 'vazio' ){
+        requisitarPagina("containerEdicao", `editar_produto_admin.php`)
 
-function clickProduto(id){
-alert("Id atual: "+ id)
-window.location = 'area_controle.php?acao=retornaDadosProduto&id_produto='+id;
+    }
+    // METODO DE VERIFICAÇÃO SIMPLES PARA SABER SE OS CAMPOS FORAM PREENCHIDOS
+
 }
-function verificaMetodo(id, metodo){
-    console.log(id)
-    console.log(metodo)
+function requisitarPaginaEdicao(metodo, id){ // PRECISO VER ESSA QUESTÃO DOS DADOS VIM PRA CA PARA QUE EU POSSA TRATAR CORRETAMENTE FIZ NA PRESSA E FICOU CONFUSO
+
+    let  inputArquivo = document.getElementById("inputArquivo"); //ARQUIVO IMAGEM DO PRODUTO
+    let descricao = document.querySelector("#descricao").value // DESCRIÇÃO DO PRODUTO
+    let nome = document.querySelector("#nome").value // NOME DO PRODUTO
+    let categoria = document.querySelector("#categoria").value // CATEGORIA DO PRODUTO
+    let valor = document.querySelector("#valor").value // VALOR DO PRODUTO
+    let quantidade = document.querySelector("#quantidade").value //QUANTIDADE DO PRODUTO
+    let arquivo = inputArquivo.files[0]; //AQUI JA É O ARQUIVO TIPO ( FILES )
+    
+    url = "editar_produto_admin.php?acao=editarProduto&id_produto="+id
+    
+    let fd = new FormData();
+    fd.append("fotografia", arquivo)
+    fd.append("descricao", descricao)
+    fd.append("nome", nome)
+    fd.append("categoria", categoria)
+    fd.append("valor", valor)
+    fd.append("quantidade", quantidade)
+    fd.append("metodo", metodo)
+    fd.append("id_produto", id)
+    let conteudo_form = document.querySelector(`#${id}`)
+    let ajax = new XMLHttpRequest();
+    ajax.open('POST', url)
+    ajax.onreadystatechange = () =>{
+        if(ajax.readyState == 4 && ajax.status == 200){
+            conteudo_form.innerHTML = ajax.responseText
+            
+            // console.log(ajax.responseText)
+        }else if(ajax.readyState == 4 && ajax.status == 404){
+            alert("ocorreu algum erro, por favor recarregue")
+        }   
+    }
+    ajax.send(fd)
 }
 
